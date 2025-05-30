39a98de5b0e3a8481becc97521e46fdc18db6457
Oleksandr Shevchenko <oleksandrs@sapling-inc.com>
2025-05-30 17:30:50 +0300

    Remove default public key.
    Signed-off-by: Oleksandr Shevchenko <oleksandrs@sapling-inc.com>

--- src/signlic/signlic.py
+++ src/signlic/signlic.py
@@ -5,12 +5,11 @@ from Crypto.Hash import SHA256
 import logging
 logger = logging.getLogger(__name__.split('.')[0])
 
-RSA_PUBLIC_KEY = b'-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA11148jvq4hccqzbxMFFm\nPkMxBPiC0ILhjYnm782HqxJQU6TBSZTnVlCPzjOiTKFa/L8z5HpxWbd6yLjF2wJo\npV5FQIJJnej6L1nbow8FNg3a2F+RXaKQMGISr0xrZrYqyE1jmZP1dmzGFV19lTu1\nQCeUh1EGkkVexYHD/Nfc7xXMYRy7HII2lNJFfmwIoup5uPfT5BDIc2esb7ol+hIX\n8EFP31r9kNqejQ0OR+BFTKiWV/Ep02nO3TvZUn65tAZQD3YVM4Yr1OTc7XzBwmgw\nAFlynldawI7K5MU2OM9H5eVK5BoiA6XsLjfB+fIsksEyaJ5Ek20hZPsFjvP+Zwd4\nD8/Q3CWMDu2NGcYL6dkY6xq8TEW9WTAUAT67d2DmZz5dIQbALzWeUKWE4HZwlTOp\na2QPrRNi9ujJXGtBuKpTCoJ1zzBOwY/j99rXLAVqxe/RnxETpU+t5Cd+Y6XGzyax\nlmw1zzBiiiZunD+X7lYhH27lQSBZAnAyy+FPNp0U0fSaFHi310FByXyGWJwQxDZA\nc8z1g0JW7feMAaYRtPpMDuj/gnAA3PqXDjsOzqMDXdIF6+DeBy/ltvGwl6G96/g5\nRtqOXeAhxJC6s7EXGM1SKFcmMVZdgwzA6dmAjp1XZE4OqVLubsMzxU6WGQGJTVKz\n4CJhPGa8vOoOzm2NOJHgAw8CAwEAAQ==\n-----END PUBLIC KEY-----'
 class SignLic:
     """
     A class to handle signing and verifying data using RSA.
     """
-    def __init__(self, rsa_public_key=RSA_PUBLIC_KEY, rsa_private_key=None):
+    def __init__(self, rsa_public_key=None, rsa_private_key=None):
         """
         Initialize the SignLic object.
         """
